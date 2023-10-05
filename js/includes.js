a714e501d7c7837148344fcdf1c5287c52416cb9
Julie Dussaussois <j.dussaussois@gmail.com>
2023-10-05 10:07:16 +0200

    feuille de nav à part, amélioration layout, alignement photos

---
+++ js/includes.js
@@ -0,0 +1,3 @@
+import { navigation } from './navigation.js'
+
+document.getElementById('navigation').innerHTML = navigation
---
+++ js/navigation.js
@@ -0,0 +1,45 @@
+export const navigation = `
+    <div class="d-flex">
+            <div class="flex-shrink-0">
+                <img src="logoresto.avif" alt="logo restaurant" width="200">
+            </div>
+            <div class="flex-grow-1 ms-3">
+                <h1><em>Le meilleur restaurant de Belgique</em></h1>
+            </div>
+        </div>
+    <div class="row">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <!-- <a class="navbar-brand" href="index.html">
+                        <h1><em>Le meilleur restaurant de Belgique</em></h1><img src="logoresto.avif"
+                            alt="logo du restaurant martoni, forme bleue sur fond blanc représentant le groin d'un cochon"
+                            width="100">
+                    </a> -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.html">Bienvenue</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="menu.html">Notre menu</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="restaurants.html">Nos restaurants</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pictures.html">Gallerie photos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+`
+
